47f03be30e23e4f3bb64521b5c34c25b04cce240
shailesh <shaileshmandhare@gmail.com>
2019-02-14 22:55:02 -0700

    Creating repository Signed-off-by: shailesh <shaileshmandhare@gmail.com>

---
+++ src/main/java/com/shailesh/rentcloud/commons/model/Customer.java
@@ -0,0 +1,50 @@
+package com.shailesh.rentcloud.commons.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name="customer")
+public class Customer {
+
+	@Id
+	@GeneratedValue
+	int id;
+	String firstName;
+	String lastName;
+	String zipCode;
+	String dlNumber;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getDlNumber() {
+		return dlNumber;
+	}
+	public void setDlNumber(String dlNumber) {
+		this.dlNumber = dlNumber;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/shailesh/rentcloud/commons/model/Vehicle.java
@@ -0,0 +1,53 @@
+package com.shailesh.rentcloud.commons.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="vehicle")
+public class Vehicle{
+	
+	@Id
+	@GeneratedValue
+	int id;
+	String make;
+	String Model;
+	String year;
+	String type;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return Model;
+	}
+	public void setModel(String model) {
+		Model = model;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
